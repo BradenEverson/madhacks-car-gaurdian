fa315da655475039386bd1d7fc5e221429385501
Braden Everson <bradeneverson@gmail.com>
2024-11-10 00:53:12 -0600

    Make alarm go crazy

--- firmware/src/peripheral.rs
+++ firmware/src/peripheral.rs
@@ -9,10 +9,10 @@ pub fn deliver_distracted_payload(relay: &mut OutputPin) {
     for _ in 1..10 {
         println!("Turning relay ON");
         relay.set_high().unwrap();
-        std::thread::sleep(Duration::from_millis(500));
+        std::thread::sleep(Duration::from_millis(50));
 
         println!("Turning relay OFF");
         relay.set_low().unwrap();
-        std::thread::sleep(Duration::from_millis(500));
+        std::thread::sleep(Duration::from_millis(50));
     }
 }
